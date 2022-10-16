12825433d3bf737d62970f44d27908c856dd5bd3
Olga <o.smolyakova30@gmail.com>
2022-10-16 13:43:44 -0500

    added alert

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const button = document.querySelector('#btn');
 button.addEventListener("click", calculateAmount);
 
+const buttonTip = document.querySelector("#addTip");
+buttonTip.addEventListener("click", showTip);
+
+function showTip(e){
+    e.preventDefault();
+    tip.style.display = "block";
+}
+
 function calculateAmount(e) {
     e.preventDefault();
     const bill = document.querySelector("#bill").value;
     const people = document.querySelector("#people").value;
     const tip = document.querySelector("#tip").value;
 
-    if (bill === "" || people === ""){
-        alert("error")
+    if (bill === "" || people === "" || people < 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Enter your information!',
+          })
     }
 
     let amountPerPerson = bill/people;
-    document.querySelector("#dividedBill").textContent = amountPerPerson;
+    document.querySelector("#dividedBill").textContent = amountPerPerson.toFixed(2);
 
     let tipPerPerson = bill * tip / people;
-    document.querySelector("#dividedTip").textContent = tipPerPerson;
+    document.querySelector("#dividedTip").textContent = tipPerPerson.toFixed(2);
 
     let totalAmountPerPerson = amountPerPerson + tipPerPerson;
-    document.querySelector("#dividedTotal").textContent = totalAmountPerPerson;
+    document.querySelector("#dividedTotal").textContent = totalAmountPerPerson.toFixed(2);
 
     
 
